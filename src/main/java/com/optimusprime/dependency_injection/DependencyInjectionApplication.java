6ac83e061d7cefb10e52869fc7a735b5bbfba93c
Menik <menikdisney@gmail.com>
2019-02-12 11:28:14 +0600

    Access to application context

--- src/main/java/com/optimusprime/dependency_injection/DependencyInjectionApplication.java
+++ src/main/java/com/optimusprime/dependency_injection/DependencyInjectionApplication.java
@@ -1,13 +1,18 @@
 package com.optimusprime.dependency_injection;
 
+import com.optimusprime.dependency_injection.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DependencyInjectionApplication.class, args);
+
+		ApplicationContext applicationContext = SpringApplication.run(DependencyInjectionApplication.class, args);
+		MyController myController = (MyController) applicationContext.getBean("myController");
+		myController.hello();
 	}
 
 }
