2c4d1d26ea85e645a99f1481ae1cbd8c2fcbdb43
Menik <menikdisney@gmail.com>
2019-02-13 12:21:03 +0600

    We can give Profile for a bean and activate it

--- src/main/java/com/optimusprime/dependency_injection/services/PrimaryGreetingServiceImpl.java
+++ src/main/java/com/optimusprime/dependency_injection/services/PrimaryGreetingServiceImpl.java
@@ -1,9 +1,11 @@
 package com.optimusprime.dependency_injection.services;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("en")
 @Primary
 public class PrimaryGreetingServiceImpl implements GreetingService {
 
--- src/main/java/com/optimusprime/dependency_injection/services/PrimarySpanishGreetingServiceImpl.java
+++ src/main/java/com/optimusprime/dependency_injection/services/PrimarySpanishGreetingServiceImpl.java
@@ -1,4 +1,17 @@
 package com.optimusprime.dependency_injection.services;
 
-public class PrimarySpanishGreetingServiceImpl {
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("es")
+@Primary
+public class PrimarySpanishGreetingServiceImpl implements  GreetingService{
+
+
+    @Override
+    public String sayGreeting() {
+        return "This is spanish greeting service!";
+    }
 }
