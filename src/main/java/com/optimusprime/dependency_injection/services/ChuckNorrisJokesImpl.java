5aacd08fcebc569fc8e68519fa659101610acefa
Menik <menikdisney@gmail.com>
2019-02-18 12:14:41 +0600

    Java configuration Correct version

--- src/main/java/com/optimusprime/dependency_injection/services/ChuckNorrisJokesImpl.java
+++ src/main/java/com/optimusprime/dependency_injection/services/ChuckNorrisJokesImpl.java
@@ -1,20 +1,21 @@
 package com.optimusprime.dependency_injection.services;
 
 import com.optimusprime.dependency_injection.config.ChuckNorrisConfig;
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChuckNorrisJokesImpl implements ChuckNorrisJokes {
 
-    private ChuckNorrisConfig chuckNorrisConfig;
+    private ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public ChuckNorrisJokesImpl(ChuckNorrisConfig chuckNorrisConfig) {
-        this.chuckNorrisConfig = chuckNorrisConfig;
+    public ChuckNorrisJokesImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
     public String getRandomQuote() {
-        String joke = chuckNorrisConfig.getCheckNorrisBean().getRandomQuote();
+        String joke = chuckNorrisQuotes.getRandomQuote();
         return joke;
     }
 }
